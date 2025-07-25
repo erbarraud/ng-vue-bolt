0e2ee594643e4d704b9e1d53b79dd5b1e8f27a14
erbarraud <154258392+erbarraud@users.noreply.github.com>
2025-07-25 11:39:21 +0200

    Make selected action buttons more neutral

--- src/views/ProductionOrders.vue
+++ src/views/ProductionOrders.vue
@@ -188,7 +188,7 @@
                         size="sm" 
                         class="w-16 h-6" 
                         :disabled="!order.canStart" 
-                        variant="outline"
+                       variant="secondary"
                         :title="order.canStart ? 'Start Order' : 'Setup Required'"
                       >
                         <Play v-if="order.canStart" class="w-3 h-3" />
@@ -196,17 +196,17 @@
                       </Button>
                       <div class="flex space-x-0.5">
                         <Button 
-                          variant="outline"
+                         variant="ghost"
                           size="icon" 
-                          class="w-8 h-5 text-gray-600 border-gray-300 hover:bg-gray-50" 
+                         class="w-8 h-5 text-gray-600 hover:bg-gray-100" 
                           title="Edit Order"
                         >
                           <Edit class="w-3 h-3" />
                         </Button>
                         <Button 
-                          variant="outline"
+                         variant="ghost"
                           size="icon" 
-                          class="w-8 h-5 text-gray-600 border-gray-300 hover:bg-gray-50" 
+                         class="w-8 h-5 text-gray-600 hover:bg-gray-100" 
                           title="View Details"
                         >
                           <Eye class="w-3 h-3" />
