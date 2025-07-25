b5b2d1673ecb0dda67e09d42571e6534efae0b85
erbarraud <154258392+erbarraud@users.noreply.github.com>
2025-07-25 10:12:08 +0200

    Force build process to exit properly

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  build: {
+    minify: 'esbuild',
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
