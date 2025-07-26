9bf2428a72977a465b493ad94c3c226fc0a2c846
erbarraud <154258392+erbarraud@users.noreply.github.com>
2025-07-26 14:10:24 +0200

    Updated BoardFinder.vue

--- src/views/BoardFinder.vue
+++ src/views/BoardFinder.vue
@@ -116,8 +116,9 @@
               placeholder="Max"
               class="flex-1 border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
             />
-      </div>
+          </div>
         </div>
+      </div>
 
       <!-- Filter Summary -->
       <div v-if="hasActiveFilters" class="mt-4 p-3 bg-emerald-50 rounded-lg border border-emerald-200">
